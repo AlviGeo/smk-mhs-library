145f457659b7f78fe6bebcae467a9df4ccbaa494
AlviGeo <alvigeovan29@gmail.com>
2022-06-17 23:59:40 +0700

    add upload image

--- react-template/src/components/datatable/Datatable.jsx
+++ react-template/src/components/datatable/Datatable.jsx
@@ -2,12 +2,53 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { collection, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import {db} from "../../firebase-config"
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
 
-  const handleDelete = (id) => {
+  useEffect(() => {
+    // const fetchData = async () => {
+    //   let list = [];
+    //   try {
+    //     const querySnapshot = await getDocs(collection(db, "users"));
+    //     querySnapshot.forEach((doc) => {
+    //       list.push({id: doc.id, ...doc.data()});
+    //       // doc.data() is never undefined for query doc snapshots
+    //       // console.log(doc.id, " => ", doc.data());
+    //     });
+    //     setData(list);
+    //     console.log(list);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
+    // fetchData()
+    const unsub = onSnapshot(collection(db, "users"), (snapShot) => {
+      let list = [];
+      snapShot.docs.forEach(doc => {
+        list.push({id:doc.id, ...doc.data()});
+      })
+      setData(list)
+    }, (error) => {
+      console.log(error);
+    })
+    return () => {
+      unsub();
+    }
+  }, []);
+
+  console.log(data);
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteDoc(doc(db, "users", id));
+    } catch (err) {
+      console.log(err);
+    }
     setData(data.filter((item) => item.id !== id));
   };
 
