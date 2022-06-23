75f0c24bd7ed2662f5a3ddda8bd33b1b28975df4
AlviGeo <alvigeovan29@gmail.com>
2022-06-23 14:38:54 +0700

    fetch modal based on id and fix css

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,25 +1,101 @@
-import React from "react";
+import React, {useContext} from "react";
 import project1 from "../images/projects/project1.jpg";
 
-function Modal({ id, title, description }) {
+// Import Context
+import {AuthContext} from "../../context/AuthContext";
+
+import { Link } from "react-router-dom";
+import coverbook from "../../components/images/projects/coverbook.jpg";
+
+
+function Modal({ id, title, description, author, publisher, status,img, timestamp, book }) {
+  const {currentUser} = useContext(AuthContext);
   return (
-    <div className="col-lg-4 m-2">
+    <div className="col-lg-3 m-2">
       <div className="project-img-container row">
         <img
           className="img-fluid book-detail"
           src={project1}
           alt="project-img"
           data-toggle="modal"
-          data-target="#toggleBook"
+          data-target={`#${id}`}
         />
         {/* Description */}
         <div className="project-item-info d-flex mx-auto">
           <div className="project-item-info-content">
-            <h4 href="project-item-title">title</h4>
-            <p>Keterangan Buku</p>
+            <h4 href="project-item-title">{title}</h4>
+            <p>{description}</p>
           </div>
         </div>
       </div>
+
+      {/* Toggle Modal */}
+      <div
+                className="modal fade"
+                id={id}
+                tabIndex={-1}
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div
+                  className="modal-dialog modal-dialog-centered"
+                  role="document"
+                >
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Book Detail
+                      </h5>
+                      <button
+                        type="button"
+                        className="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <span aria-hidden="true">×</span>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <div className="row">
+                        <div className="col-6">
+                          <img
+                            className="modal-book-detail"
+                            src={img}
+                            width="150"
+                            height="180"
+                            alt="Book Cover"
+                          />
+                        </div>
+                        <div className="col-6 text-left">
+                          <h4>{title}</h4>
+                          <p className="project-cat">Author: {author} </p>
+                          <p className="project-cat">Publisher: {publisher}</p>
+                          <p className="project-cat">Date Published: {} </p>
+                          <p className="project-cat">Available: {status} </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        data-dismiss="modal"
+                      >
+                        Tutup
+                      </button>
+                      {currentUser ? (
+                        <Link to="/borrowstatus" className="btn btn-primary">
+                          Pinjam
+                        </Link>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End of Modal */}
     </div>
   );
 }
--- frontend/src/pages/user/CategoryDetail.js
+++ frontend/src/pages/user/CategoryDetail.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 // Import Data from Firebase
-import { collection, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
-import {db} from "../../firebase-config"
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 // Import layouts
 import Footer from "../../layouts/Footer/Footer";
 import Navbar from "../../layouts/Navbar/Navbar";
 import SideNavbar from "../../layouts/Navbar/SideNavbar";
-
-// Import Context
-import {AuthContext} from "../../context/AuthContext";
 
 // Images
 import project1 from "../../components/images/projects/project1.jpg";
@@ -19,23 +22,30 @@ import coverbook from "../../components/images/projects/coverbook.jpg";
 import Modal from "../../components/Modal/Modal";
 
 const CategoryDetail = () => {
-  const navigate = useNavigate();
-  const {currentUser} = useContext(AuthContext);
-
   const [books, setBooks] = useState([]);
   const usersCollectionRef = collection(db, "books");
 
   useEffect(() => {
-    const getBooks = async () => {
-      const data = await getDocs(usersCollectionRef);
-      console.log(data)
-      setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    const unsub = onSnapshot(
+      collection(db, "books"),
+      (snapShot) => {
+        let list = [];
+        snapShot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        console.log(list);
+        setBooks(list);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return () => {
+      unsub();
     };
-
-    getBooks();
   }, []);
 
-
+  // Dummy data
   const staticbooks = [
     {
       id: 1,
@@ -111,81 +121,25 @@ const CategoryDetail = () => {
             <SideNavbar />
 
             <div className="col-lg-8 mb-5 mb-lg-0 order-0 order-lg-1">
-              <div className="row text-center">
+              <div className="row text-center justify-content-around">
                 {books.map((book) => (
                   <>
-                    <Modal key={book.id} id={book.id} title={book.book_title} description={book.book_description}/>
+                    <Modal
+                      key={book.id}
+                      id={book.id}
+                      title={book.book_title}
+                      description={book.book_description}
+                      author={book.book_author}
+                      publisher={book.book_publisher}
+                      status={book.book_status}
+                      img={book.book_img}
+                      timestamp={book.book_timeStamp}
+                      books={book}
+                    />
+                    {console.log(book)}
                   </>
                 ))}
               </div>
-
-              {/* Toggle Modal */}
-              <div
-                className="modal fade"
-                id="toggleBook"
-                tabIndex={-1}
-                role="dialog"
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true"
-              >
-                <div
-                  className="modal-dialog modal-dialog-centered"
-                  role="document"
-                >
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLabel">
-                        Book Detail
-                      </h5>
-                      <button
-                        type="button"
-                        className="close"
-                        data-dismiss="modal"
-                        aria-label="Close"
-                      >
-                        <span aria-hidden="true">×</span>
-                      </button>
-                    </div>
-                    <div className="modal-body">
-                      <div className="row">
-                        <div className="col-6">
-                          <img
-                            className="modal-book-detail"
-                            src={coverbook}
-                            width="150"
-                            height="180"
-                            alt="Book Cover"
-                          />
-                        </div>
-                        <div className="col-6 text-left">
-                          <h4>Judul Buku</h4>
-                          <p className="project-cat">Author: </p>
-                          <p className="project-cat">Publisher: </p>
-                          <p className="project-cat">Date Published: </p>
-                          <p className="project-cat">Available: </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="modal-footer">
-                      <button
-                        type="button"
-                        className="btn btn-secondary"
-                        data-dismiss="modal"
-                      >
-                        Tutup
-                      </button>
-                      {currentUser ? (
-                        <Link to="/booklist" className="btn btn-primary">
-                          Pinjam
-                        </Link>
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* End of Modal */}
 
               {/* Pagination Slides */}
               <hr />
