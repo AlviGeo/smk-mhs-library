8817545454ba68b4f81a0e6fd62c28e160962cbf
AlviGeo <alvigeovan29@gmail.com>
2022-06-25 19:55:43 +0700

    change login design & add password toggle

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,9 +1,10 @@
 import React, { useContext, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Alert } from "react-bootstrap";
+import "../../components/css/Login.css";
 
 // Alert
 import Swal from "sweetalert2";
+import { Alert } from "react-bootstrap";
 
 // Import Context
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -14,24 +15,30 @@ function Login() {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordShown, setPasswordShown] = useState(false);
 
   const navigate = useNavigate();
-
   const { dispatch } = useContext(AuthContext);
+
+  // Show and Hide Password
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
 
   const handleLogin = (e) => {
     e.preventDefault();
 
     signInWithEmailAndPassword(auth, email, password)
+      .catch((err) => {
+        setError(true);
+        Swal.fire("", "Login Failed!", "error");
+      })
       .then((userCredential) => {
         const user = userCredential.user;
         dispatch({ type: "LOGIN", payload: user });
         navigate("/");
         window.location.reload();
-      })
-      .then(Swal.fire("", "Success Login!", "success"))
-      .catch((err) => {
-        setError(true);
+        Swal.fire("", "Success Login!", "success");
       });
   };
 
@@ -45,7 +52,7 @@ function Login() {
               <div className="container">
                 <div className="row">
                   <div className="col-lg-7 col-xl-6 mx-auto">
-                    <h3 className="display-4">LOGIN</h3> <br />
+                    <h3 className="display-4">LOG IN</h3> <br />
                     {error && (
                       <Alert variant="danger">Wrong email or password!</Alert>
                     )}
@@ -54,28 +61,38 @@ function Login() {
                         <input
                           type="email"
                           autoFocus
-                          className="Email form-control rounded-pill border-0 shadow-sm px-4"
+                          className="Email form-control border-0 shadow-sm px-4"
                           placeholder="Email address"
                           required
                           onChange={(e) => setEmail(e.target.value)}
                         />
                       </div>
 
-                      <div className="form-group mb-3">
+                      <div className="input-group form-group mb-3">
                         <input
-                          type="password"
-                          className="password form-control rounded-pill border-0 shadow-sm px-4 text-danger"
+                          type={passwordShown ? "text" : "password"}
+                          className="password form-control border-0 shadow-sm px-4 "
                           placeholder="Password"
                           required
                           onChange={(e) => setPassword(e.target.value)}
                         />
+                        <div className="input-group-prepend cursor-pointer">
+                          <span className="input-group-text">
+                            <i
+                              onClick={togglePassword}
+                              className={`fa ${
+                                togglePassword ? "fa-eye-slash" : "fa-eye"
+                              }`}
+                            ></i>
+                          </span>
+                        </div>
                         <br />
                       </div>
 
                       <button
                         type="submit"
                         value="login"
-                        className="btn btn-danger btn-block text-uppercase mb-2 rounded-pill shadow-sm"
+                        className="btn btn-warning btn-block text-uppercase mb-2 rounded-pill shadow-sm"
                       >
                         Sign in
                       </button>
