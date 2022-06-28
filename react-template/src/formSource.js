cdc5c65a5e29dba8e33c5b5273f35d6d04d3b5a9
AlviGeo <alvigeovan29@gmail.com>
2022-06-29 03:06:12 +0700

    remove unused fields

--- react-template/src/formSource.js
+++ react-template/src/formSource.js
@@ -3,42 +3,19 @@ export const userInputs = [
     id: "username",
     label: "Username",
     type: "text",
-    placeholder: "john_doe",
-  },
-  {
-    id: "displayName",
-    label: "Name and surname",
-    type: "text",
-    placeholder: "John Doe",
+    placeholder: "admin123",
   },
   {
     id: "email",
     label: "Email",
-    type: "mail",
-    placeholder: "john_doe@gmail.com",
-  },
-  {
-    id: "phone",
-    label: "Phone",
-    type: "text",
-    placeholder: "+1 234 567 89",
+    type: "email",
+    placeholder: "admin@gmail.com",
   },
   {
     id: "password",
     label: "Password",
     type: "password",
-  },
-  {
-    id: "address",
-    label: "Address",
-    type: "text",
-    placeholder: "Elton St. 216 NewYork",
-  },
-  {
-    id: "country",
-    label: "Country",
-    type: "text",
-    placeholder: "USA",
+    placeholder: "Insert password"
   },
 ];
 
