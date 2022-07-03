fdd3f2d4621d062c226c9ed3042cdaee45682d9f
AlviGeo <alvigeovan29@gmail.com>
2022-07-03 17:05:36 +0700

    pagination still havent works

--- frontend/src/pages/user/CategoryDetail.js
+++ frontend/src/pages/user/CategoryDetail.js
@@ -10,37 +10,41 @@ import {
   doc,
   onSnapshot,
   query,
-  where
+  where,
 } from "firebase/firestore";
 import { db } from "../../firebase-config";
 
 // Import layouts
 import Footer from "../../layouts/Footer/Footer";
 import Navbar from "../../layouts/Navbar/Navbar";
+import Pagination from "../../components/Pagination/Pagination";
 
 // Images
 import Modal from "../../components/Modal/Modal";
 
 const CategoryDetail = () => {
   const [books, setBooks] = useState([]);
-  const [searchBooksTitle, setSearchBooksTitle] = useState("")
+  const [searchBooksTitle, setSearchBooksTitle] = useState("");
   const [categories, setCategory] = useState([]);
 
-  useEffect(() => {
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
 
+  useEffect(() => {
     const fetchData = async () => {
       let list = [];
       try {
         const querySnapshot = await getDocs(collection(db, "category"));
         querySnapshot.forEach((doc) => {
-          list.push({id: doc.id, ...doc.data()});
+          list.push({ id: doc.id, ...doc.data() });
         });
         setCategory(list);
       } catch (err) {
         console.log(err);
       }
     };
-    fetchData()
+    fetchData();
 
     const unsub = onSnapshot(
       collection(db, "books"),
@@ -60,10 +64,17 @@ const CategoryDetail = () => {
     };
   }, []);
 
-  
-  if(books.length === 0) {
-    return (<p>Loading...</p>)
+  if (books.length === 0) {
+    return <p>Loading...</p>;
   }
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentBooks = books.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   // Dummy data
   const staticbooks = [
@@ -138,94 +149,80 @@ const CategoryDetail = () => {
       <section id="main-container" className="main-container">
         <div className="container">
           <div className="row">
-          <div className="col-lg-4 order-1 order-lg-0">
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Cari Buku.."
-          aria-label="Search"
-          onChange={event => {setSearchBooksTitle(event.target.value)}}
-        />
-        <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="sidebar sidebar-left">
-        <div className="widget">
-          <h3 className="widget-title mt-4">Kategori</h3>
-          {categories.map((category) => (
-          <ul className="arrow nav nav-tabs">
-            <li>
-              <a href="/#">{category.category_name}</a>
-              {/* <a href="/#">Sementara</a> */}
-            </li>
-          </ul>
-          ))}
-        </div>
-        
-      </div>
-      {/* Sidebar end */}
-    </div>
+            <div className="col-lg-4 order-1 order-lg-0">
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Cari Buku.."
+                  aria-label="Search"
+                  onChange={(event) => {
+                    setSearchBooksTitle(event.target.value);
+                  }}
+                />
+                <button
+                  className="btn btn-outline-warning my-2 my-sm-0"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+              <div className="sidebar sidebar-left">
+                <div className="widget">
+                  <h3 className="widget-title mt-4">Kategori</h3>
+                  {categories.map((category) => (
+                    <ul className="arrow nav nav-tabs">
+                      <li>
+                        <a href="/#">{category.category_name}</a>
+                        {/* <a href="/#">Sementara</a> */}
+                      </li>
+                    </ul>
+                  ))}
+                </div>
+              </div>
+              {/* Sidebar end */}
+            </div>
 
             <div className="col-lg-8 mb-5 mb-lg-0 order-0 order-lg-1">
               <div className="row text-center justify-content-around">
-                {books.filter((book) => {
-                  if(searchBooksTitle === "")  {
-                    return book 
-                  } else if (book.book_title.toLowerCase().includes(searchBooksTitle.toLowerCase())) {
-                    return book
-                  } 
-                }).map((book, key) => (
-                  <>
-                  {/* {console.log(book)} */}
-                    <Modal
-                      key={key}
-                      id={book.id}
-                      title={book.book_title}
-                      description={book.book_description}
-                      author={book.book_author}
-                      publisher={book.book_publisher}
-                      total={book.book_total}
-                      img={book.img}
-                      timestamp={book.book_timeStamp}
-                      books={book}
-                    />
-                  </>
-                ))}
+                {currentBooks
+                  .filter((book) => {
+                    if (searchBooksTitle === "") {
+                      return book;
+                    } else if (
+                      book.book_title
+                        .toLowerCase()
+                        .includes(searchBooksTitle.toLowerCase())
+                    ) {
+                      return book;
+                    }
+                  })
+                  .map((book, key) => (
+                    <>
+                      {/* {console.log(book)} */}
+                      <Modal
+                        key={key}
+                        id={book.id}
+                        title={book.book_title}
+                        description={book.book_description}
+                        author={book.book_author}
+                        publisher={book.book_publisher}
+                        total={book.book_total}
+                        img={book.img}
+                        timestamp={book.book_timeStamp}
+                        books={book}
+                      />
+                    </>
+                  ))}
               </div>
 
               {/* Pagination Slides */}
               <hr />
-              <nav className="paging" aria-label="Page navigation example">
-                <ul className="pagination justify-content-center">
-                  <li className="page-item">
-                    <a className="page-link" href="/#">
-                      <i className="fas fa-angle-double-left" />
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="/#">
-                      1
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="/#">
-                      2
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="/#">
-                      3
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="/#">
-                      <i className="fas fa-angle-double-right" />
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+              <Pagination
+                postsPerPage={postsPerPage}
+                totalBooks={books.length}
+                paginate={paginate}
+              />
             </div>
           </div>
         </div>
