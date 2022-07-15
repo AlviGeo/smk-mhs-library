23b5f781aa8dd1cb067e19ccef25d5449cbefd61
AlviGeo <alvigeovan29@gmail.com>
2022-07-16 02:20:50 +0700

    change login design

--- react-template/src/pages/login/Login.jsx
+++ react-template/src/pages/login/Login.jsx
@@ -2,11 +2,24 @@ import { useContext, useState } from "react"
 import "./login.scss"
 import {useNavigate} from "react-router-dom";
 import Swal from 'sweetalert2';
+import logo from "../../components/images/mhs-logo.png";
+
+// Material UI
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Alert from '@mui/material/Alert';
 
 // Firebase
 import { signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../../firebase-config";
 import { AuthContext } from "../../context/AuthContext";
+
+const theme = createTheme();
 
 const Login = () => {
   const [error, setError] = useState(false);
@@ -31,16 +44,61 @@ const Login = () => {
       )
     }).catch((err) => {setError(true)});
   }
-  
+
   return (
-    <div className="login">
-      <form onSubmit={handleLogin}>
-        {error && <span>Wrong email or password!</span>}
-        <input type="email" placeholder="email" onChange={e=> setEmail(e.target.value)}/>
-        <input type="password" placeholder="password" onChange={e=> setPassword(e.target.value)}/>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <img src={logo} alt="MHS" style={{height: "120px", width: "350px", marginBottom: "5px"}} />
+          <Typography component="h1" variant="h5">
+            Sign in (Admin)
+          </Typography>
+          <br />
+          <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+          {error && <Alert severity="error">Wrong Email or Password</Alert>}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={e=> setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={e=> setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{backgroundColor: "#B22D31"}}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
 
